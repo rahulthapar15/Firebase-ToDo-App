43dbcc97c0141a0c7534b0a50135de8d3cdb785f
Rahul Thapar <rahthap@gmail.com>
2017-11-01 12:49:51 +0530

    Adding card animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var text = document.getElementById("card_text");
+// Sync Text area for card and input box
 
+var text = document.getElementById("card_text");
 $("#todo").keyup(function () {
     text.value = this.value;
 });
