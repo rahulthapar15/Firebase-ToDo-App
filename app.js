db39bcc29acc560acf88d903916d3f0db1b8fb8c
Rahul Thapar <rahthap@gmail.com>
2017-11-02 09:52:47 +0530

    Firebase Push : DONE

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ var text = document.getElementById("card_text");
 $("#todo").keyup(function () {
     text.value = this.value;
 });
+
+//Creating FIREBASE reference
+
+var ref = firebase.database().ref();
+
+//Pushing data into firebase
+var submitBtn = document.getElementById("submitBtn");
+var todo = document.getElementById("todo");
+function submitCard(){
+
+    if (!$.trim($("#card_text").val())) {
+        // textarea is empty or contains only white-space
+        window.alert("Text area cannot be empty!");
+    }else{
+        var mText = text.value;
+        ref.child("List").push().set(mText);
+        window.alert(text.value);
+        text.value = "";
+        todo.value = "";
+    }
+}
